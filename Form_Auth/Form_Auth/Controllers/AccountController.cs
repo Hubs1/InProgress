44017b26e0cbf97619c1bd2e104bec122de77ffb
GitHub <hsbhel17@gmail.com>
2019-11-21 19:38:01 +0530

    FormAuthentication

---
+++ Form_Auth/Form_Auth/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Form_Auth.Models;//access UserFields
+using System.Web.Security;// access form authentication (cookies)
+
+namespace Form_Auth.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login() // ModelClass- UserFields
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(UserFields userFields)
+        {
+            using (var userContext = new OfficeContext()) //made connection with DbContext and check if user exist in database on not
+            {
+                bool isValid = userContext.Users.Any(u => u.UserName == userFields.UserName && u.Password == userFields.Password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(userFields.UserName, false); // Persistant cookie (true to create a persistant cookie (one that saved across browser sessions) otherwise false) works on remember checkbox 
+                    return RedirectToAction("Index", "Office");
+                }
+
+                ModelState.AddModelError("", "Invalid username and password. Try again!!!!!");
+            }
+            return View();
+        }
+        public ActionResult Signup() // ModelClass- User [User.cs access from .edmx]
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(User user)
+        {
+            using(var userContext = new OfficeContext()) //made connection with DbContext and save in database
+            {
+                userContext.Users.Add(user);
+                userContext.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Form_Auth/Form_Auth/Models/UserFields.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Form_Auth.Models
+{
+    public class UserFields
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
